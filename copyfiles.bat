3649efd882d43a591fad8d8a7229762e2281497c
Rafael Torres Escobar <torresc.rafael@gmail.com>
2023-06-28 11:29:32 -0600

    fileanmes were changed

--- copyfiles.bat
+++ copyfiles.bat
@@ -1,5 +1,5 @@
-set sourcefile=espinosa-de-los-monteros-ruiz_luis-gonzalo_00453398.docx 
-set filenames=perez-palma_susana-montserrat_00478755.docx quiniones-ortega_eder-saul_00468669.docx saavedra-mercado_erwin-john_00460594.docx
+set sourcefile=source_file.docx 
+set filenames=file1.docx file2.docx file3.docx
 
 for %%F in (%filenames%) do (
     copy %sourcefile% %%F
